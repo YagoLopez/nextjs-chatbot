ca6dc3c70adbde4ff6926ea75bbc594045ebe20a
Yago López <yago.lopez@gmail.com>
2024-12-28 20:49:55 +0100

    summarize web page with rag

---
+++ src/app/api/test/route.ts
@@ -0,0 +1,87 @@
+import { ChatMistralAI } from "@langchain/mistralai";
+import { MistralAIEmbeddings } from "@langchain/mistralai";
+import { MemoryVectorStore } from "langchain/vectorstores/memory";
+import { RecursiveCharacterTextSplitter } from "@langchain/textsplitters";
+
+import { CheerioWebBaseLoader } from "@langchain/community/document_loaders/web/cheerio";
+import { ChatPromptTemplate } from "@langchain/core/prompts";
+import { pull } from "langchain/hub";
+import { Document } from "@langchain/core/documents";
+import { Annotation } from "@langchain/langgraph";
+import { StateGraph } from "@langchain/langgraph";
+
+export const maxDuration = 30;
+
+export async function GET() {
+
+  const llm = new ChatMistralAI({
+    model: "mistral-large-latest",
+    temperature: 0
+  });
+
+  const embeddings = new MistralAIEmbeddings({
+    model: "mistral-embed"
+  });
+
+  const vectorStore = new MemoryVectorStore(embeddings);
+
+  const cheerioLoader = new CheerioWebBaseLoader(
+    "https://lilianweng.github.io/posts/2023-06-23-agent/",
+    {
+      selector: "p",
+    }
+  );
+
+  const docs = await cheerioLoader.load();
+
+  const splitter = new RecursiveCharacterTextSplitter({
+    chunkSize: 1000,
+    chunkOverlap: 200,
+  });
+  const splittedDocuments = await splitter.splitDocuments(docs);
+
+  await vectorStore.addDocuments(splittedDocuments)
+
+
+  const promptTemplate = await pull<ChatPromptTemplate>("rlm/rag-prompt");
+
+  const InputStateAnnotation = Annotation.Root({
+    question: Annotation<string>,
+  });
+
+  const StateAnnotation = Annotation.Root({
+    question: Annotation<string>,
+    context: Annotation<Document[]>,
+    answer: Annotation<string>,
+  });
+
+  const retrieve = async (state: typeof InputStateAnnotation.State) => {
+    const retrievedDocs = await vectorStore.similaritySearch(state.question);
+    return { context: retrievedDocs };
+  };
+
+  const generate = async (state: typeof StateAnnotation.State) => {
+    const docsContent = state.context.map((doc) => doc.pageContent).join("\n");
+    const messages = await promptTemplate.invoke({
+      question: state.question,
+      context: docsContent,
+    });
+    const response = await llm.invoke(messages);
+    return { answer: response.content };
+  };
+
+  const graph = new StateGraph(StateAnnotation)
+    .addNode("retrieve", retrieve)
+    .addNode("generate", generate)
+    .addEdge("__start__", "retrieve")
+    .addEdge("retrieve", "generate")
+    .addEdge("generate", "__end__")
+    .compile();
+
+  let inputs = { question: "Make a sumary of this blog post" };
+
+  const result = await graph.invoke(inputs);
+
+  return new Response(JSON.stringify(result, null, 2));
+}
+
