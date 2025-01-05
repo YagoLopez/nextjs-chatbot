0fa0ad9d25ef3daa7dc44892799ecb7f0d3cc651
Yago López <yago.lopez@gmail.com>
2025-01-06 00:17:24 +0100

    Logo image

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -55,8 +55,14 @@ export default function TwoBlockPage() {
   return (
     <div className="min-h-screen bg-gray-100 p-4 sm:p-6 md:p-8 flex flex-col">
       <Card className="flex-grow w-full max-w-6xl mx-auto my-4 sm:my-6 md:my-8 flex flex-col shadow-xl">
-        <CardTitle className="[text-shadow:_1px_3px_5px_#838383] text-2xl font-bold text-center flex justify-center mt-7 text-blue-600">
-          <Image src="/chat-logo.png" alt="logo" className="w-10 h-10 mx-2" />
+        <CardTitle className="text-2xl font-bold bg-gradient-to-r from-orange-700 via-blue-500 to-green-400 text-transparent bg-clip-text animate-gradient mt-7 flex justify-center">
+          <Image
+            width="40"
+            height="10"
+            src="/chat-logo.png"
+            alt="logo"
+            className="mx-2"
+          />
           Chat with a web page. By Yago López
         </CardTitle>
         <CardContent className="p-4 sm:p-6 flex-grow flex flex-col">
