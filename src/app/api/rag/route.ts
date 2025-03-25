2e66431d185a08dc47265e500880e9ce6b1c0109
Yago López <yago.lopez@gmail.com>
2025-03-26 00:19:12 +0100

    Change strings

--- src/app/api/rag/route.ts
+++ src/app/api/rag/route.ts
@@ -36,14 +36,16 @@ export async function POST(req: NextRequest) {
 
   await vectorStore.addDocuments(chunkDocuments);
 
-  const template = `Use the following pieces of context to answer the question at the end.
+  const template = `Use the following provided text to answer the question asked at the end.
     If you don't know the answer, just say that you couldn't find the information in the provided context. 
     Don't try to make enough information to answer, don't try to make up an answer.
     Keep the answer as concise as possible.
     
+    - This is the provided text:
     {context}
     
-    Question: {question}
+    - This is the question you should answer: 
+    {question}
     
     Helpful Answer:`;
 
