24ca343b8c89f9c064142aa7014c3425733a06c1
YagoLopez <yago.lopez@gmail.com>
2025-10-01 18:19:24 +0200

    create scraper service

--- src/app/api/scrape/route.ts
+++ src/app/api/scrape/route.ts
@@ -1,45 +1,22 @@
-import { chromium } from "playwright";
+import { scrapingService } from "@/services/scraping-service";
 import { URLS } from "@/lib/constants";
 
 // Force the function to be dynamic and not cached
 export const dynamic = "force-dynamic";
 
 export async function GET() {
-  let browser = null;
-
-  console.log("Starting scraper...");
-
   try {
-    browser = await chromium.launch({ headless: true });
-    const page = await browser.newPage();
-    await page.goto(URLS[1]);
+    const scrappedText = await scrapingService.scrapePage(URLS[1]);
 
-    console.log("Page loaded. Scraping content...");
-
-    const heading = await page.locator("body").textContent();
-
-    console.log("Scraping complete.");
-
-    // Return the scraped data as plain text
-    return new Response(heading || "No heading found.", {
+    return new Response(scrappedText, {
       status: 200,
-      headers: {
-        "Content-Type": "text/plain",
-      },
+      headers: { "Content-Type": "text/plain" },
     });
   } catch (error) {
-    console.error("Error during scraping:", error);
-    // Also return the error as plain text
+    console.error("Error in API route:", error);
     return new Response("Failed to scrape the page.", {
       status: 500,
-      headers: {
-        "Content-Type": "text/plain",
-      },
+      headers: { "Content-Type": "text/plain" },
     });
-  } finally {
-    if (browser) {
-      await browser.close();
-      console.log("Browser closed.");
-    }
   }
 }
