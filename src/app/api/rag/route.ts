4d6943825455afc855aee6e70c427a753d2b38f6
YagoLopez <yago.lopez@gmail.com>
2025-10-04 22:04:10 +0200

    test 3

--- src/app/api/rag/route.ts
+++ src/app/api/rag/route.ts
@@ -1,19 +1,19 @@
 import { ChatMistralAI } from "@langchain/mistralai";
-import { MistralAIEmbeddings } from "@langchain/mistralai";
-import { MemoryVectorStore } from "langchain/vectorstores/memory";
-import { RecursiveCharacterTextSplitter } from "@langchain/textsplitters";
-
-import { CheerioWebBaseLoader } from "@langchain/community/document_loaders/web/cheerio";
+// import { MistralAIEmbeddings } from "@langchain/mistralai";
+// import { MemoryVectorStore } from "langchain/vectorstores/memory";
+// import { RecursiveCharacterTextSplitter } from "@langchain/textsplitters";
+//
+// import { CheerioWebBaseLoader } from "@langchain/community/document_loaders/web/cheerio";
 import { ChatPromptTemplate } from "@langchain/core/prompts";
 
 import { LangChainAdapter } from "ai";
-import { type NextRequest /*NextResponse*/ } from "next/server";
+// import { type NextRequest /*NextResponse*/ } from "next/server";
 
-export async function POST(req: NextRequest) {
-  const { prompt: userInput } = await req.json();
-  const remoteUrl =
-    req.nextUrl.searchParams.get("url") ||
-    "https://lilianweng.github.io/posts/2023-06-23-agent/";
+export async function POST(/*req: NextRequest*/) {
+  // const { prompt: userInput } = await req.json();
+  // const remoteUrl =
+  //   req.nextUrl.searchParams.get("url") ||
+  //   "https://lilianweng.github.io/posts/2023-06-23-agent/";
 
   const llm = new ChatMistralAI({
     streamUsage: false,
@@ -22,32 +22,31 @@ export async function POST(req: NextRequest) {
     temperature: 0,
   });
 
-  const embeddings = new MistralAIEmbeddings({ model: "mistral-embed" });
+  // const embeddings = new MistralAIEmbeddings({ model: "mistral-embed" });
+  //
+  //  const vectorStore = new MemoryVectorStore(embeddings);
+  //
+  //  const cheerioLoader = new CheerioWebBaseLoader(remoteUrl, { selector: "p" });
+  //
+  //  const htmlText = await cheerioLoader.load();
+  //
+  //  const splitter = new RecursiveCharacterTextSplitter({
+  //    chunkSize: 3000,
+  //    chunkOverlap: 400,
+  //  });
+  // const chunkDocuments = await splitter.splitDocuments(htmlText);
 
-  const vectorStore = new MemoryVectorStore(embeddings);
-
-  const cheerioLoader = new CheerioWebBaseLoader(remoteUrl, { selector: "p" });
-
-  const htmlText = await cheerioLoader.load();
-
-  const splitter = new RecursiveCharacterTextSplitter({
-    chunkSize: 3000,
-    chunkOverlap: 400,
-  });
-  const chunkDocuments = await splitter.splitDocuments(htmlText);
-
-  await vectorStore.addDocuments(chunkDocuments);
+  // await vectorStore.addDocuments(chunkDocuments);
 
   const template = `Given this text: "{context}" I want you to give an answer this question "{question}".
   
     If you don't know the answer, just say that you couldn't find any information related in the provided context. 
     Don't try to make enough information to answer, don't try to make up an answer.
     Keep the answer as concise as possible.`;
-
   const promptTemplate = ChatPromptTemplate.fromMessages([["user", template]]);
 
-  const relatedDocs = await vectorStore.similaritySearch(userInput);
-  console.log("relateddocs", relatedDocs);
+  // const relatedDocs = await vectorStore.similaritySearch(userInput);
+  // console.log("relateddocs", relatedDocs);
 
   // const mergedRelatedDocs = relatedDocs
   //   .map((doc) => doc.pageContent)
