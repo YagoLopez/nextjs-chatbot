8a7ebfe4218d9a92cba9d6b13325ac60240a8e67
YagoLopez <yago.lopez@gmail.com>
2025-10-05 14:08:52 +0200

    test last version ai sdk 1

--- src/app/api/rag2/route.ts
+++ src/app/api/rag2/route.ts
@@ -6,8 +6,8 @@ export const maxDuration = 60;
 export async function POST() {
   const result = streamText({
     model: mistral("mistral-large-latest"),
-    prompt: "Write a poem about embedding models.",
+    prompt: "Esto es una prueba",
   });
 
-  return result.toDataStreamResponse();
+  return result.toUIMessageStreamResponse();
 }
