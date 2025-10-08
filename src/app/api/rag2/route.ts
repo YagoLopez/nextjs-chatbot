4caf537ff45481de6a6c7cfe53f157a4647d14aa
YagoLopez <yago.lopez@gmail.com>
2025-10-08 18:49:18 +0200

    wip: add openrouter

--- src/app/api/rag2/route.ts
+++ src/app/api/rag2/route.ts
@@ -1,8 +1,10 @@
+import { createOpenAI, openai } from "@ai-sdk/openai";
 import { mistral } from "@ai-sdk/mistral";
 import { streamText } from "ai";
 import { MistralAIEmbeddings } from "@langchain/mistralai";
 import { MemoryVectorStore } from "langchain/vectorstores/memory";
 import { RecursiveCharacterTextSplitter } from "@langchain/textsplitters";
+import { openrouter } from "@openrouter/ai-sdk-provider";
 
 import { CheerioWebBaseLoader } from "@langchain/community/document_loaders/web/cheerio";
 
@@ -39,17 +41,36 @@ export async function POST(req: NextRequest) {
     .map((doc) => doc.pageContent)
     .join("\n");
 
+  const openai = createOpenAI({
+    apiKey:
+      "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",
+    // headers: {
+    //   "header-name": "header-value",
+    // },
+  });
+
   const template = createPromptTemplate(
     userInput,
     mergedRelatedDocs,
     remoteUrl,
   );
-
   const result = streamText({
-    model: mistral("mistral-medium-latest"),
+    model: openrouter("openai/gpt-4o"),
     prompt: template,
     system: systemPrompt,
   });
+
+  // const result = streamText({
+  //   model: openai("gpt-5"),
+  //   prompt: template,
+  //   system: systemPrompt,
+  // });
+
+  // const result = streamText({
+  //   model: mistral("mistral-medium-latest"),
+  //   prompt: template,
+  //   system: systemPrompt,
+  // });
 
   return result.toUIMessageStreamResponse();
 }
