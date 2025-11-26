66cb40031b5020ef6196d61f73dc6915e43a7a5d
YagoLopez <yago.lopez@gmail.com>
2025-11-26 20:32:27 +0100

    chore: remove dead code

--- src/app/api/rag/route.ts
+++ src/app/api/rag/route.ts
@@ -14,7 +14,7 @@ export async function POST(req: NextRequest) {
 
   const remoteUrl = req.nextUrl.searchParams.get("url") || "";
 
-  const prompt = `Given this url: ${remoteUrl} and this user question: ${userInput}, I want you to give me a response based on the content of the URL. The response should be brief and concise.\n\n    If you don't know the answer or cannot find the information in the provided URL, state that you could not find the relevant information in the provided context. Do not make up an answer.`;
+  const prompt = `Given this url: ${remoteUrl} and this user question: ${userInput}, I want you to give me a response based on the content of the URL. The response should be brief and concise.\n\n If you don't know the answer or cannot find the information in the provided URL, state that you could not find the relevant information in the provided context. Do not make up an answer.`;
 
   const result = streamText({
     model: google("gemini-2.5-flash"),
