43c7677a98115127dd4ea8015951809482f42fa9
Yago López <yago.lopez@gmail.com>
2025-11-26 20:27:41 +0100

    use-google-tools-for-rag (#15)
    * feat: use GoogleGenAI lib for rag
    
    * fix: typescript error
    
    * chore: code clean up
    
    * fix: remove any
    
    * fix: cr comments
    
    * fix: add guard clause to rag endpoint

--- src/app/api/rag/route.ts
+++ src/app/api/rag/route.ts
@@ -1,56 +1,27 @@
-import { streamText } from "ai";
-import { MistralAIEmbeddings } from "@langchain/mistralai";
-import { MemoryVectorStore } from "langchain/vectorstores/memory";
-import { RecursiveCharacterTextSplitter } from "@langchain/textsplitters";
-
-import { CheerioWebBaseLoader } from "@langchain/community/document_loaders/web/cheerio";
-
 import { type NextRequest } from "next/server";
-import { createPromptTemplate, systemPrompt } from "@/lib/utils";
 import { google } from "@ai-sdk/google";
+import { streamText, Tool } from "ai";
 
 export const runtime = "edge";
 
 export const maxDuration = 60;
 
 export async function POST(req: NextRequest) {
+  if (!req.headers.get("content-type")?.includes("application/json")) {
+    return new Response("Invalid content type", { status: 400 });
+  }
   const { prompt: userInput } = await req.json();
 
   const remoteUrl = req.nextUrl.searchParams.get("url") || "";
 
-  const embeddings = new MistralAIEmbeddings({ model: "mistral-embed" });
-
-  const vectorStore = new MemoryVectorStore(embeddings);
-
-  const cheerioLoader = new CheerioWebBaseLoader(remoteUrl, { selector: "p" });
-
-  const htmlText = await cheerioLoader.load();
-
-  const splitter = new RecursiveCharacterTextSplitter({
-    chunkSize: 3000,
-    chunkOverlap: 400,
-  });
-
-  const chunkDocuments = await splitter.splitDocuments(htmlText);
-
-  await vectorStore.addDocuments(chunkDocuments);
-
-  const relatedDocs = await vectorStore.similaritySearch(userInput);
-
-  const mergedRelatedDocs = relatedDocs
-    .map((doc) => doc.pageContent)
-    .join("\n");
-
-  const template = createPromptTemplate(
-    userInput,
-    mergedRelatedDocs,
-    remoteUrl,
-  );
+  const prompt = `Given this url: ${remoteUrl} and this user question: ${userInput}, I want you to give me a response based on the content of the URL. The response should be brief and concise.\n\n    If you don't know the answer or cannot find the information in the provided URL, state that you could not find the relevant information in the provided context. Do not make up an answer.`;
 
   const result = streamText({
     model: google("gemini-2.5-flash"),
-    prompt: template,
-    system: systemPrompt,
+    prompt,
+    tools: {
+      url_context: google.tools.urlContext({}) as Tool<unknown, unknown>,
+    },
   });
 
   return result.toUIMessageStreamResponse();
